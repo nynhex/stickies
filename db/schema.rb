3271b246df2c5c87386f98240d7b2effe812c968
Eric Perez <ericricardoperez@gmail.com>
2014-03-21 16:04:08 -0700

    sticky note model and associations

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140321172846) do
+ActiveRecord::Schema.define(version: 20140321230033) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "stickies", force: true do |t|
+    t.string   "title"
+    t.string   "body"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "stickies", ["user_id"], name: "index_stickies_on_user_id", using: :btree
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
