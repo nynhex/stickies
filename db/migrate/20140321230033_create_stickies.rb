32da8bfda2c1959e9b7310cd8fe09200695e773d
Eric Perez <ericricardoperez@gmail.com>
2014-03-24 11:07:09 -0700

    changed the body of sticky to text column in schema

--- db/migrate/20140321230033_create_stickies.rb
+++ db/migrate/20140321230033_create_stickies.rb
@@ -2,7 +2,7 @@ class CreateStickies < ActiveRecord::Migration
   def change
     create_table :stickies do |t|
       t.string :title
-      t.string :body
+      t.text :body
       t.belongs_to :user, index: true
 
       t.timestamps
--- db/schema.rb
+++ db/schema.rb
@@ -18,7 +18,7 @@ ActiveRecord::Schema.define(version: 20140321230033) do
 
   create_table "stickies", force: true do |t|
     t.string   "title"
-    t.string   "body"
+    t.text     "body"
     t.integer  "user_id"
     t.datetime "created_at"
     t.datetime "updated_at"
