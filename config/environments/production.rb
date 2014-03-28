22d9768b70b0be6f2472cad6bbcd669793cf5938
Eric Perez <ericricardoperez@gmail.com>
2014-03-27 22:40:34 -0700

    typo

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,4 @@
- fStickies::Application.configure do
+ Stickies::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
